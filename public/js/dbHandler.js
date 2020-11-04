27404e1cdfb85fe1304ff77c29db8043eff48831
Bárbara Boechat <barbs.boechat@gmail.com>
2020-11-04 18:42:53 -0300

    Update dbHandler.js

--- public/js/dbHandler.js
+++ public/js/dbHandler.js
@@ -13,24 +13,26 @@
 var database = firebase.database();
 
 function get_data_test() {
-    firebase.database().ref('users').once('value').then(function (snapshot) {
-        // var values = snapshot.val();
-        if(snapshot.exists()) {
-            var content = '';
-            snapshot.forEach(item => {
-                console.log(item.val())
-                    content +='<tr>';
-                    content += '<td>' + item.val().name + '</td>';
-                    content += '<td>' + item.val().tempoFase1 + '</td>';
-                    content += '<td>' + item.val().fase2 + '</td>';
-                    content += '<td>' + item.val().fase3 + '</td>';
-                    content += '<td> None </td>';
-    
-                    content += '</tr>';
-            });
-            $('#users-table').append(content);
-        }
-    });
+  firebase.database().ref('users').once('value').then(function (snapshot) {
+      if(snapshot.exists()) {
+        var content = '';
+        snapshot.forEach(item => {
+          if(item.val().name != undefined){
+            content +='<tr>';
+            content += '<td>' + item.val().name + '</td>';
+            content += '<td>' + item.val().tempoFase1 + '</td>';
+            content += '<td>' + item.val().fase2 + '</td>';
+            content += '<td>' + item.val().fase3 + '</td>';
+            content += '<td> None </td>';
+
+            content += '</tr>';
+          }  
+        });
+
+        $('#users-table').append(content);
+
+      }
+  });
 }
 
 get_data_test();
